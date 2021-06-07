b6b5d8edfce26c11d90140c11fbd9e1a6407294e
pranka mondal <priankanew0011@gmail.com>
2021-06-07 11:58:44 +0600

    new change

--- ERPManagementSystem/Models/QuotationLineItem.cs
+++ ERPManagementSystem/Models/QuotationLineItem.cs
@@ -19,6 +19,7 @@ namespace ERPManagementSystem.Models
         public decimal Quantity { get; set; }
         public decimal TotalCost { get; set; }
         public string Description { get; set; }
+        public string ImgPath { get; set; }
         //Navigation
         public Guid ProductId { get; set; }
         public Product Product { get; set; }
