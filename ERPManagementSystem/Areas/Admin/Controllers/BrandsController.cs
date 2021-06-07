31df18cfcfe24f8ab35a3fa41fd0b24557435158
Faysal Ahmed <faysalahmed3859@outlook.com>
2021-06-07 10:00:47 +0600

    Add Brand and subcategory Controller

---
+++ ERPManagementSystem/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,124 @@
+﻿using ERPManagementSystem.Data;
+using ERPManagementSystem.Extensions;
+using ERPManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static ERPManagementSystem.Extensions.Helper;
+
+namespace ERPManagementSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BrandsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int pg)
+        {
+            var brand = _context.Brands.Where(c => c.Status == "Enable");
+            const int pageSize = 10;
+            if (pg < 1)
+            {
+                pg = 1;
+            }
+            var resCount = brand.Count();
+            var pager = new Pager(resCount, pg, pageSize);
+            int resSkip = (pg - 1) * pageSize;
+            var data = brand.Skip(resSkip).Take(pager.PageSize);
+            ViewBag.Pager = pager;
+            return View(await data.ToListAsync());
+        }
+
+        //AddOrEdit
+        [NoDirectAccess]
+        public async Task<IActionResult> AddOrEdit(Guid id)
+        {
+            if (id == Guid.Parse("00000000-0000-0000-0000-000000000000"))
+            {
+                return View(new Brand());
+            }
+
+            else
+            {
+                var brand = await _context.Brands.FindAsync(id);
+                if (brand == null)
+                {
+                    return NotFound();
+                }
+                return View(brand);
+            }
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(Guid id, Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                Brand entity;
+                if (id == Guid.Parse("00000000-0000-0000-0000-000000000000"))
+                {
+                    entity = new Brand();
+                    entity.Id = Guid.NewGuid();
+                    entity.Name = brand.Name;
+                    entity.Status = brand.Status;
+                    _context.Add(entity);
+                    await _context.SaveChangesAsync();
+                }
+
+                else
+                {
+                    try
+                    {
+                        entity = await _context.Brands.FindAsync(brand.Id);
+                        entity.Name = brand.Name;
+                        entity.Status = brand.Status;
+                        _context.Update(entity);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException ex)
+                    {
+
+                    }
+                }
+                var brandData = _context.Brands.Where(c => c.Status == "Enable");
+                int pg = 1;
+                const int pageSize = 10;
+                if (pg < 1)
+                {
+                    pg = 1;
+                }
+                var resCount = brandData.Count();
+                var pager = new Pager(resCount, pg, pageSize);
+                int resSkip = (pg - 1) * pageSize;
+                var data = brandData.Skip(resSkip).Take(pager.PageSize);
+                ViewBag.Pager = pager;
+                return Json(new { isValid = true, html = Helper.RenderRazorViewToString(this, "_ViewAllBrand", data) });
+            }
+            return Json(new { isValid = false, html = Helper.RenderRazorViewToString(this, "AddOrEdit", brand) });
+
+        }
+        //delete category
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var brand = await _context.Brands.FindAsync(id);
+            brand.Status = "Disable";
+            await _context.SaveChangesAsync();
+
+            return Json(new { isValid = true, html = Helper.RenderRazorViewToString(this, "_ViewAllBrand", _context.Brands.Where(c => c.Status == "Enable").ToList()) });
+
+        }
+    }
+}
