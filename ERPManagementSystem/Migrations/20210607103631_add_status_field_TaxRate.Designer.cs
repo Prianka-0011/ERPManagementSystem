905789f2b6fd08808d5b26a367240acb158449dc
Faysal Ahmed <faysalahmed3859@outlook.com>
2021-06-07 17:31:04 +0600

    Add.TaxRate

--- ERPManagementSystem/Migrations/20210607103631_add_status_field_TaxRate.Designer.cs
+++ ERPManagementSystem/Migrations/20210607103631_add_status_field_TaxRate.Designer.cs
@@ -4,14 +4,16 @@ using ERPManagementSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPManagementSystem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210607103631_add_status_field_TaxRate")]
+    partial class add_status_field_TaxRate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ERPManagementSystem/Models/TaxRate.cs
+++ ERPManagementSystem/Models/TaxRate.cs
@@ -10,5 +10,6 @@ namespace ERPManagementSystem.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public int Rate { get; set; }
+        public string Status { get; set; }
     }
 }
