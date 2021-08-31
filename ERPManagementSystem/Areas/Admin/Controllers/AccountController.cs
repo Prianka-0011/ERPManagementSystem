b927e3e7ece9ee70bf5d9549a85b966692b8e7eb
prianka@techintel.com.bd <prianka@techintel.com.bd>
2021-08-31 22:40:20 +0600

    hello git

--- ERPManagementSystem/Areas/Admin/Controllers/AccountController.cs
+++ ERPManagementSystem/Areas/Admin/Controllers/AccountController.cs
@@ -22,11 +22,10 @@ namespace ERPManagementSystem.Areas.Admin.Controllers
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<AccountController> _logger;
-        private readonly IEmailSender _emailSender;
         private readonly RoleManager<IdentityRole> _roleManager;
 
         public AccountController(
-           ILogger<AccountController> logger, IEmailSender emailSender, RoleManager<IdentityRole> roleManager,
+           ILogger<AccountController> logger, RoleManager<IdentityRole> roleManager,
            ApplicationDbContext context, UserManager<ApplicationUser> userManager,
            SignInManager<ApplicationUser> signInManager, IWebHostEnvironment webHostEnvironment)
         {
@@ -36,7 +35,6 @@ namespace ERPManagementSystem.Areas.Admin.Controllers
             _signInManager = signInManager;
             _userManager = userManager;
             _roleManager = roleManager;
-            _emailSender = emailSender;
             _logger = logger;
         }
         [HttpGet]
@@ -58,7 +56,7 @@ namespace ERPManagementSystem.Areas.Admin.Controllers
                     var user = _context.Users.FirstOrDefault(c => c.UserName == login.UserName || c.Email == login.UserName);
                     var role = _context.UserRoles.FirstOrDefault(c => c.UserId == user.Id);
                     var roleType = _context.Roles.FirstOrDefault(c => c.Id == role.RoleId);
-                    if (roleType.Name == "Admin")
+                    if (roleType.Name == "Admin" || roleType.Name == "Employee")
                     {
                         //   return Json(new { isValid = false, html = Helper.RenderRazorViewToString(this, "Index", _context.PaymentBankingType.ToList()) });
                         return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
